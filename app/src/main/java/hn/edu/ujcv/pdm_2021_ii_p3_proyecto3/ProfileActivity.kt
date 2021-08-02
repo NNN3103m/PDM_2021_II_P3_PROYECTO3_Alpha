7fd806a9bdb594bb0d565b344808e70dc352ce12
nnn31 <nnn3103m@gmail.com>
2021-08-01 19:24:32 -0600

    ProfileActivity.kt and activity_profile.xml done

---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p3_proyecto3/ProfileActivity.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.pdm_2021_ii_p3_proyecto3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
